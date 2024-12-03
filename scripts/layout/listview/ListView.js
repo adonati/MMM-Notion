27971ea68958ecc2c01afcdaef7f1c92fa964867
adonati <adonati@ascendhit.com>
2024-12-02 20:06:52 -0600

    Update ListView.js
    Add ability to edit lists

--- scripts/layout/listview/ListView.js
+++ scripts/layout/listview/ListView.js
@@ -6,9 +6,10 @@
  */
 
 class ListView {
-	constructor(databaseProps) {
+	constructor(databaseProps,rootObject) {
 		this.databaseProps = databaseProps
 		this.wrapper = this.createListWrapper()
+		this.rootObject = rootObject
 		this.init()
 	}
 
@@ -31,8 +32,60 @@ class ListView {
 				database_id: this.databaseProps.id,
 				displayElementTitle: this.databaseProps.layout.displayElementTitle,
 			}
-			const element = new ListViewElement(props, config)
+			const element = new ListViewElement(props, config, this.rootObject)
 			this.wrapper.appendChild(element.wrapper)
+		})
+		this.createNewElementPlaceholder()
+	}
+
+	createNewElementPlaceholder() {
+		const newItemWrapper = document.createElement("div")
+		newItemWrapper.id = "mmm-notion-listview-element"
+
+		const newItemContainer = document.createElement("div")
+		newItemContainer.id = "mmm-notion-listview-titleContainer"
+
+		const newItem = document.createElement("div")
+		newItem.id = "mmm-notion-listview-title"
+
+		newItem.classList = "notionListItem notionNewItem"
+		newItem.innerText = "Add an item..."
+		newItemContainer.appendChild(newItem)
+		
+		// Additional properties to edit
+		const buttonBar = document.createElement("div")
+		buttonBar.className = "editItemButtonBar hideMe"
+		const titleEditOK = document.createElement("i")
+		titleEditOK.className = "editButton fa-regular fa-circle-check"
+		titleEditOK.style = "color:green"
+		buttonBar.appendChild(titleEditOK)
+
+		const titleEditCancel = document.createElement("i")
+		titleEditCancel.className = "editButton fa-solid fa-xmark"
+		titleEditCancel.style = "color:red"
+		buttonBar.appendChild(titleEditCancel)
+		
+		newItemContainer.appendChild(buttonBar)
+		newItemWrapper.appendChild(newItemContainer)
+		this.wrapper.appendChild(newItemWrapper)
+
+		// Event handlers
+		const self = this
+		const rowButtonBar = buttonBar
+		newItem.addEventListener("click", function() {
+			self.rootObject.makeEditable(newItem,rowButtonBar)
+		})
+		titleEditOK.addEventListener("click", function() {
+			self.rootObject.makeUnEditable(newItem,rowButtonBar)
+			self.rootObject.addRow(newItem.innerText);
+			// Refresh the display after the addition has taken effect (might have to bump up time?)
+			setTimeout(function() {
+				self.rootObject.refreshContents()
+			},500);
+		})
+		titleEditCancel.addEventListener("click", function() {
+			self.rootObject.makeUnEditable(newItem,rowButtonBar)
+			self.rootObject.refreshContents()
 		})
 	}
 }
